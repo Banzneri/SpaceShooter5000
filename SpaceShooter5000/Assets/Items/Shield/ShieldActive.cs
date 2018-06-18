32ced0342d93557a09d74b66d36b8e1aeeff2617
Unknown <pasi.hanhijarvi@gmail.com>
2018-06-18 07:20:10 +0300

    Add shield bar

--- SpaceShooter5000/Assets/Items/Shield/ShieldActive.cs
+++ SpaceShooter5000/Assets/Items/Shield/ShieldActive.cs
@@ -4,28 +4,35 @@ using UnityEngine;
 
 public class ShieldActive : MonoBehaviour {
 	[SerializeField] private float _shieldHealth = 100;
+	[SerializeField] private ShieldBar _shieldBar;
 	
+	private Transform _barPanel;
 	private Material _material;
 	private Color _originalColor;
 	private PlayerTakeDamage _playerDamage;
+	private float _maxShield;
+
+	public float Health
+	{
+		get { return _shieldHealth; }
+		set { _shieldHealth = value; }
+	}
 
 	void Start () {
 		_material = GetComponent<Renderer>().material;
 		_originalColor = _material.color;
 		_playerDamage = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerTakeDamage>();
+		_barPanel = GameObject.FindGameObjectWithTag("BarPanel").transform;
 		_playerDamage.Shielded = true;
+		_shieldBar = Instantiate(_shieldBar, transform.position, Quaternion.identity, _barPanel) as ShieldBar;
+		_maxShield = _shieldHealth;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Color lerpedColor = Color.Lerp(_originalColor, Color.red, Mathf.PingPong(Time.time * 10, 1));
 		_material.color = lerpedColor;
-
-		if (_shieldHealth <= 0)
-		{
-			_playerDamage.Shielded = false;
-			Destroy(gameObject);
-		}
+		_shieldBar.SetBarValue(_shieldHealth / _maxShield);
 	}
 
 	private void OnTriggerEnter(Collider other) {
@@ -34,6 +41,12 @@ public class ShieldActive : MonoBehaviour {
 			Bullet bullet = other.GetComponent<Bullet>();
 			_shieldHealth -= bullet.Damage;
 			Destroy(other.gameObject);
+			if (_shieldHealth <= 0)
+			{
+				_playerDamage.Shielded = false;
+				Destroy(gameObject);
+				Destroy(_shieldBar.gameObject);
+			}
 		}
 	}
 }
---
+++ SpaceShooter5000/Assets/UI/ShieldBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShieldBar : MonoBehaviour {
+	[SerializeField] private GameObject _barPanel;
+	
+	private float _maxShield;
+	private Slider _shieldBarSlider;
+
+	private void Start() {
+		_shieldBarSlider = GetComponent<Slider>();
+	}
+
+	public void SetBarValue(float value)
+	{
+		_shieldBarSlider.value = value;
+	}
+}
